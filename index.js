e312e4aa9af88000f08b1f302101cabc28d8e2be
Kavyasr <kavyasri28@gmail.com>
2026-05-05 16:49:56 +0530

    cicd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello')
 })
-app.listen(3000,()=>{
-    console.log("server is runing in 3000 port")
+app.listen(3000, () => {
+    console.log("server is runing in 3000 port hi aws run the server")
 })
